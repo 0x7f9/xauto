7537f78922cb240a08b19495e258f8becc35422a
0x7f9 <141240295+0x7f9@users.noreply.github.com>
2025-08-06 12:21:59 +1200

    Fix support for running as standalone script

--- xauto/bootstrap/cleanup_bootstrap.py
+++ xauto/bootstrap/cleanup_bootstrap.py
@@ -1,7 +1,21 @@
 #!/usr/bin/env python3
 
-from bootstrap.build import cleanup_bootstrap
+# run clean up with:
+# python -m pyile.bootstrap.cleanup_bootstrap
+
+# or 
+
+# cd pyile_root\pyile\bootstrap
+# python cleanup_bootstrap.py
+
+import sys 
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..")))
+
+from pyile.bootstrap.build import cleanup_bootstrap
+
 
 print("Cleaning up bootstrap...")
 cleanup_bootstrap()
 print("Done.")
+
