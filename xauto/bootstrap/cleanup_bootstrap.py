bde0340d2a5eb2cb27442baf34f02327240ff07c
0x7f9 <141240295+0x7f9@users.noreply.github.com>
2025-08-06 12:29:40 +1200

    Update cleanup_bootstrap.py

--- xauto/bootstrap/cleanup_bootstrap.py
+++ xauto/bootstrap/cleanup_bootstrap.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python3
 
 # run clean up with:
-# python -m pyile.bootstrap.cleanup_bootstrap
+# python -m xauto.bootstrap.cleanup_bootstrap
 
 # or 
 
-# cd pyile_root\pyile\bootstrap
+# cd xauto_root\xauto\bootstrap
 # python cleanup_bootstrap.py
 
 import sys 
 import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..")))
 
-from pyile.bootstrap.build import cleanup_bootstrap
+from xauto.bootstrap.build import cleanup_bootstrap
 
 
 print("Cleaning up bootstrap...")
